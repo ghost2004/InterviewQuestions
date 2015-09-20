63e9bf6feac73866c12e86431a1e12668d7943ff
ghost2004 <liutg@hotmail.com>
2015-09-19 21:45:52 -0700

    Map design, not sure about it

---
+++ src/PureStorage/MapDesigner.java
@@ -0,0 +1,13 @@
+package PureStorage;
+/*
+ * Design a Map<Integer, Integer>
+ * 
+ * Match following requirement
+ * 
+ * add: O(1)  deletion: O(1)  lookup: O(1)  clear:O(1)  iterate: O(number of elements)¡£
+ * 
+ * Assume the memory is big enough and there is no collision in the map
+ */
+public class MapDesigner {
+
+}
