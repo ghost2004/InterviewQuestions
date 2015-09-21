10ef310fa10286585154830bb9accb7d3a3d186d
ghost2004 <liutg@hotmail.com>
2015-09-20 22:31:21 -0700

    Map with time first version

---
+++ src/Uber/MapWithTime.java
@@ -0,0 +1,33 @@
+package Uber;
+/*
+ * Design a map , implement following methods
+ * 
+ * put(key,value,time)  -- put a pair into map with time stamp
+ * get(key, time)  -- get the value before that time
+ * 
+ */
+import java.util.*;
+public class MapWithTime {
+    private HashMap<Integer, TreeMap<Integer, Integer>> entryMap;
+    public MapWithTime() {
+        entryMap = new HashMap<Integer, TreeMap<Integer, Integer>> ();
+    }
+    
+    public void put(int key, int value, int time) {
+        TreeMap<Integer,Integer> map = entryMap.get(key);
+        if (map == null) {
+            map = new TreeMap<Integer, Integer>();
+            entryMap.put(time, map);
+        }
+        map.put(time, value);
+    }
+    
+    public Integer get(int key, int time) {
+        TreeMap<Integer,Integer> map = entryMap.get(key);
+        if (map == null)
+            return null;
+        Map.Entry<Integer, Integer> entry =  map.lowerEntry(time);
+        return entry.getValue();
+    }
+
+}
