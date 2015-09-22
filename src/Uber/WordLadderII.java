04d06b99642cab79a588384291a71f6de165ccf6
ghost2004 <liutg@hotmail.com>
2015-09-21 17:29:59 -0700

    ?

---
+++ src/Uber/WordLadderII.java
@@ -0,0 +1,103 @@
+package Uber;
+/*
+ * Given two words (beginWord and endWord), and a dictionary's word list, find all shortest 
+ * transformation sequence(s) from beginWord to endWord, such that:
+
+Only one letter can be changed at a time
+Each intermediate word must exist in the word list
+For example,
+
+Given:
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log"]
+Return
+  [
+    ["hit","hot","dot","dog","cog"],
+    ["hit","hot","lot","log","cog"]
+  ]
+Note:
+All words have the same length.
+All words contain only lower case alphabetic characters.
+ */
+import java.util.*;
+public class WordLadderII {
+    class WordNode {
+        public String word;
+        public int step;
+        public WordNode prev;
+        
+        public WordNode(String w, int s, WordNode p) {
+            this.word = w;
+            this.step =s;
+            this.prev = p;
+        }
+    }
+    
+    public List<List<String>> findLadders(String beginWord, String endWord, Set<String> wordList) {
+        List<List<String>> result = new ArrayList<List<String>>();
+        if (beginWord.equals(endWord)) {
+            List<String> item = new ArrayList<String>();
+            item.add(beginWord);
+            result.add(item);
+            return result;
+        }
+        LinkedList<WordNode> queue = new LinkedList<WordNode>();
+        HashSet<String> visited = new HashSet<String>();
+        HashSet<String> unvisited = new HashSet<String>();
+        
+        unvisited.addAll(wordList);
+        unvisited.add(endWord);
+        
+        queue.offer(new WordNode(beginWord, 0, null));
+        int minStep = 0;
+        int preStep = 0;
+        while (!queue.isEmpty()) {
+            WordNode n = queue.poll();
+            String key = n.word;
+            if (key.equals(endWord)) {
+                if (minStep == 0)
+                    minStep = n.step;
+                if (n.step <= minStep) {
+                    if (n.step < minStep) {
+                        minStep = n.step;
+                        result.clear();
+                    }
+                    List<String> item = new ArrayList<String>();
+                    WordNode cur = n;
+                    while (cur != null) {
+                        item.add(0, cur.word);
+                        cur = cur.prev;
+                    }
+                    result.add(item);
+                }
+                continue;
+            }
+            
+            if (preStep < n.step) {
+                unvisited.removeAll(visited);
+            }
+            
+            preStep = n.step;
+            char array[] = key.toCharArray();
+            for (int i = 0; i < array.length; i++) {
+                for (char a = 'a'; a <= 'z';a++) {
+                    char temp = array[i];
+                    if(array[i]!=a){
+                        array[i]=a;
+                    }
+
+                    String next = new String(array);
+                    if (unvisited.contains(key)) {
+                        queue.add(new WordNode(next, preStep+1, n));
+                        visited.add(next);
+                    }
+                    array[i] = temp;
+                }
+            }
+            
+        }
+        
+        return result;
+    }
+}
