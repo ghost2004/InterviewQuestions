ba83f55fe82262f9f7defb7d2c45c62967aa8ba2
ghost2004 <liutg@hotmail.com>
2015-09-25 22:21:51 -0700

    Validate number

--- src/Linkedin/ValidNumber.java
+++ src/Linkedin/ValidNumber.java
@@ -11,9 +11,55 @@ Some examples:
 Note: It is intended for the problem statement to be ambiguous. You should gather all requirements up front before implementing one.
  */
 public class ValidNumber {
-    public boolean isNumber(String s) {
+    public static boolean isNumber(String s) {
         if (s == null || s.length() == 0)
             return false;
-        return true;
+        boolean dotFlag = false;
+        boolean eFlag = false;
+        boolean numFlag = false;
+        String p = s.trim();
+        if (p.length() == 0)
+            return false;
+        int idx = 0;
+        if (p.charAt(idx) == '+' || p.charAt(idx) == '-')
+            idx++;
+        for (; idx < p.length(); idx++) {
+            char key = p.charAt(idx);
+            if (Character.isDigit(key)) {
+                numFlag = true;
+            }
+            else if (key == 'e') {
+                if (!eFlag) {
+                    if (!numFlag)
+                        return false;
+                    numFlag = false;
+                    eFlag = true;
+                    continue;
+                } 
+                return false;
+            }else if (key == '.') {
+                if (!dotFlag) {
+                    if (eFlag)
+                        return false;
+                    dotFlag = true;
+                    continue;
+                } 
+                return false;
+            } else if (key == '+' || key == '-') {
+                if (p.charAt(idx-1) != 'e')
+                    return false;
+            } else
+                return false;
+                
+        }
+        
+
+        
+        return numFlag;
+    }
+    
+    public static void main(String args[]) {
+        String t = "  tere ";
+        System.out.println(t.trim());
     }
 }
