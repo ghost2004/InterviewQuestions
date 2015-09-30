1aa1c822061383b99d132fcaafd3b272b462f3ea
ghost2004 <liutg@hotmail.com>
2015-09-30 09:59:38 -0700

    comment

--- src/Linkedin/LevelDepthSum.java
+++ src/Linkedin/LevelDepthSum.java
@@ -2,10 +2,10 @@ package Linkedin;
 /*
  * Given a multi-level list, return the sum of all level depth
  * 
- *  given the list {{1,1},2,{1,1}}£¬  return 10 
+ *  given the list {{1,1},2,{1,1}}, return 10 
  *  (four 1's at depth 2, one 2 at depth 1).
  *  
- *  given {1,{4,{6}}} £¬return 27
+ *  given {1,{4,{6}}}, return 27
  *   (one 1 at depth 1, one 4 at depth 2, and one 6 at depth 3)
  */
 public class LevelDepthSum {
