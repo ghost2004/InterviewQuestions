1ea76179d36ae7ae6855d9964a2584159dad7ff7
ghost2004 <liutg@hotmail.com>
2015-10-05 22:24:47 -0700

    Key map designer version1

--- src/PureStorage/MapDesigner.java
+++ src/PureStorage/MapDesigner.java
@@ -8,6 +8,46 @@ package PureStorage;
  * 
  * Assume the memory is big enough and there is no collision in the map
  */
+import java.util.*;
 public class MapDesigner {
-
+    private class MapNode{
+        public Integer value;
+        public Integer version;
+    }
+    private HashMap<Integer, MapNode> map;
+    private Integer version;
+    
+    public MapDesigner() {
+        this.map = new HashMap<Integer, MapNode>();
+        this.version = 1;
+    }
+    
+    public void put(Integer key, Integer value) {
+        MapNode node = map.get(key);
+        if (node == null) {
+            node = new MapNode();
+        }
+        node.value = value;
+        node.version = this.version;
+        map.put(key, node);
+    }
+    
+    public Integer get(Integer key) {
+        MapNode node = map.get(key);
+        if (node == null)
+            return null;
+        if (node.version != this.version)
+            return null;
+        
+        return node.value;
+    }
+    
+    public void clear() {
+        this.version++;
+    }
+    
+    public void remove(Integer key) {
+        map.remove(key);
+    }
+   
 }
