c8fa6be8fc69e36eaf43f7d7a2741a91e7040907
ghost2004 <liutg@hotmail.com>
2017-07-25 16:34:21 -0700

    Interval data structure

---
+++ src/Common/Interval.java
@@ -0,0 +1,32 @@
+package Common;
+
+public class Interval {
+    int start;
+    int end;
+    Interval() { start = 0; end = 0; }
+    Interval(int s, int e) { start = s; end = e; }
+    
+    public static Interval[] getFromArray(int start[], int end[]){
+        if (start == null || end == null || start.length != end.length)
+            return null;
+        
+        Interval[] out = new Interval[start.length];
+        
+        for (int i = 0; i < start.length; i++) { 
+            out[i] = new Interval(start[i], end[i]);
+        }
+        
+        return out;
+    }
+    
+    public static Interval[] getFromArray(int a[][]) {
+        if (a == null || a.length < 1 || a[0].length != 2)
+            return null;
+        Interval[] out = new Interval[a.length];
+        
+        for (int i = 0; i < a.length; i++) {
+            out[i] = new Interval(a[i][0], a[i][1]);
+        }
+        return out;
+    }
+}
