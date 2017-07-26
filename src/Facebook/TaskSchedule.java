57ff9b434636ebde05314a63f4fab090cd2ef94e
ghost2004 <liutg@hotmail.com>
2017-07-25 23:41:00 -0700

    Facebook task schedule issue

---
+++ src/Facebook/TaskSchedule.java
@@ -0,0 +1,13 @@
+package Facebook;
+/*
+ * We have a list of tasks, they will be run in sequence. For each same tasks, there is a cool down time in the CPU
+ * 
+ * Given a task sequence and interval between 2 same tasks, return the running time of the tasks
+ * 
+ * For Example , the interval for task B is 3,  the running time of task list BB will be 5 ( 1+ 3(cool down) + 1)
+ * 
+ * Follow up: Given a task list and interval, return the sequence that requires minimum time.
+ */
+public class TaskSchedule {
+
+}
