8d7ecec222b7542057a270eed4bcafd62c8ee014
qa-mbp-b-2 <qa-mbp-b-2@10.1.100.7>
2017-07-27 16:35:09 -0700

    changes for same interval

--- src/Facebook/TaskScheduleTest.java
+++ src/Facebook/TaskScheduleTest.java
@@ -10,10 +10,14 @@ public class TaskScheduleTest {
     public void testRunningTime() {
         int task1[] = {1, 1, 2, 1};
         int interval1[] = { 2, 2 ,2};
+        int itvl1 =2;
         int task2[] = {1,2,3,1,2,3};
         int interval2[] = { 3, 3 ,3, 3};
+        int itvl2 = 3;
         assertEquals(7, sch.runningTime(task1, interval1));
         assertEquals(7, sch.runningTime(task2, interval2));
+        assertEquals(7, sch.runTime(task1, itvl1));
+        assertEquals(7, sch.runTime(task2, itvl2));
         
         
     }
