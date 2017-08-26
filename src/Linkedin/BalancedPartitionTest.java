e9d57dcb7da6bd3454e2723412ee0ea408e7fc57
qa-mbp-b-2 <qa-mbp-b-2@192.168.1.114>
2017-08-26 09:58:26 -0700

    Balanced partition solution I

---
+++ src/Linkedin/BalancedPartitionTest.java
@@ -0,0 +1,21 @@
+package Linkedin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BalancedPartitionTest {
+
+    @Test
+    public void testFindMinV1() {
+        BalancedPartition b = new BalancedPartition();
+        int array1[] = {1, 6, 11, 5};
+        int array2[] = {1, 5, 11, 5};
+        int array3[] = {1, 2, 3, 5, 6};
+        assertEquals(1, b.findMinV1(array1));
+        assertEquals(0, b.findMinV1(array2));
+        assertEquals(1, b.findMinV1(array3));
+        
+    }
+
+}
