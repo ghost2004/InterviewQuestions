b855999362c8f99296a1abc2f195abf26b926dc1
ghost2004 <liutg@hotmail.com>
2017-08-26 22:39:51 -0700

    KClosestPoints

---
+++ src/Linkedin/KClosestPoints.java
@@ -0,0 +1,8 @@
+package Linkedin;
+/*
+ * Given an array containing N points find the K closest points to the origin in the 2D plane. 
+ * You can assume K is much smaller than N and N is very large.
+ */
+public class KClosestPoints {
+
+}
