a70cb44733a734c3bcb79cab56295bee9265ecff
qa-mbp-b-2 <qa-mbp-b-2@192.168.1.114>
2017-08-29 14:36:20 -0700

    Leetcode 53. Maximum Subarray

---
+++ src/Linkedin/MaxSubarray.java
@@ -0,0 +1,27 @@
+package Linkedin;
+/*
+ * Leetcode 53. Maximum Subarray
+ * Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+ */
+public class MaxSubarray {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length < 1)
+            return 0;
+        int max = nums[0];
+        int sum = max;
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (sum < 0) 
+                sum = nums[i];
+            else 
+                sum += nums[i];
+            max = Math.max(max, sum);
+        }
+        
+        
+        return max;
+    }
+}
