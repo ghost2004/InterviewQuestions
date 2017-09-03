2002dfff762bc71884c2b5a0ed18fbc29df7d5ea
ghost2004 <liutg@hotmail.com>
2017-09-03 11:58:34 -0700

    Leetcode 671. Second Minimum Node In a Binary Tree

--- src/Linkedin/TournamentTree.java
+++ src/Linkedin/TournamentTree.java
@@ -1,26 +1,88 @@
 package Linkedin;
 /*
-* A tournament tree is a binary tree 
-* where the parent is the minimum of the two children. 
-* Given a tournament tree find the second minimum value in the tree. 
-* A node in the tree will always have 2 or 0 children. 
-* Also all leaves will have distinct and unique values. 
-*         2 
-*      /    \ 
-*     2      3 
-*    / \    /  \    
-*   4   2   5   3 
-* 
-* In this given tree the answer is 3. 
+ * Leetcode 671. Second Minimum Node In a Binary Tree
+Given a non-empty special binary tree consisting of nodes with the non-negative value, where each node in 
+this tree has exactly two or zero sub-node. If the node has two sub-nodes, then this node's value is the
+smaller value among its two sub-nodes.
+
+Given such a binary tree, you need to output the second minimum value in the set made of all the nodes' 
+value in the whole tree.
+
+If no such second minimum value exists, output -1 instead.
+
+Example 1:
+Input: 
+    2
+   / \
+  2   5
+     / \
+    5   7
+
+Output: 5
+Explanation: The smallest value is 2, the second smallest value is 5.
+Example 2:
+Input: 
+    2
+   / \
+  2   2
+
+Output: -1
+Explanation: The smallest value is 2, but there isn't any second smallest value.
  */
 
 import Common.TreeNode;
+import java.util.*;
 public class TournamentTree {
+    
+    PriorityQueue<Integer> pq;
+    public void travel(TreeNode node) {
+        if (node == null)
+            return;
+        pq.offer(node.val);
+        travel(node.left);
+        travel(node.right);
+    }
+    public int findSecondMinimumValue_pq(TreeNode root) {
+        pq = new PriorityQueue<Integer>();
+        travel(root);
+        
+        while (!pq.isEmpty() && pq.peek() == root.val)
+            pq.poll();
+        
+        if (pq.isEmpty())
+            return -1;
+        
+        return pq.peek();
+        
+    }
+    
+    private int secondMin(TreeNode node, int parent) {
+        if (node == null)
+            return Integer.MAX_VALUE;
+        if (node.val != parent)
+            return node.val;
+        int left = secondMin(node.left, node.val);
+        int right = secondMin(node.right, node.val);
+        
+        return Math.min(left, right);
+    }
+    
+    public int findSecondMinimumValue(TreeNode root) {
+        if (root == null)
+            return -1;
+        int min = secondMin(root, root.val);
+        return min == Integer.MAX_VALUE ? -1:min;
+    }
+    
+    /* Failed in leetcode
     public int secondMin(TreeNode root) {
         int min = 0;
         
         if (root.left == null || root.right == null)
             return root.val;
+        
+        if (root.left.val == root.right.val)
+            return -1;
         TreeNode node;
         if (root.left.val == root.val) {
             min = root.right.val;
@@ -30,6 +92,12 @@ public class TournamentTree {
             node = root.right;
         }
         
-        return Math.min(min, secondMin(node));
+        int c = secondMin(node);
+        
+        if (c == -1)
+            return node.val;
+        
+        return Math.min(min, c);
     }
+    */
 }
