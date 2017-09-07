0b63fb5c293e675187509ac2d6b5fd1a6ddeddd5
ghost2004 <liutg@hotmail.com>
2017-09-06 23:19:47 -0700

    Leetcode 611 Valid Triangle Number

--- src/Linkedin/TriangleSides.java
+++ src/Linkedin/TriangleSides.java
@@ -1,34 +1,48 @@
 package Linkedin;
 /*
- * 3 sides can form  a triangle when 
- *  A + B > C
- *  B + C > A
- *  A + C > B
- *  
- *  Given a list of segments and return sides can form a triangle
- *  
- *  return empty array when there is no solution
+ * Leetcode 611 Valid Triangle Number
+ * Given an array consists of non-negative integers, your task is to count the number of triplets 
+ * chosen from the array that can make triangles if we take them as side lengths of a triangle.
+
+Example 1:
+Input: [2,2,3,4]
+Output: 3
+Explanation:
+Valid combinations are: 
+2,3,4 (using the first 2)
+2,3,4 (using the second 2)
+2,2,3
+Note:
+The length of the given array won't exceed 1000.
+The integers in the given array are in the range of [0, 1000].
+
  */
 
 import java.util.*;
 public class TriangleSides {
     
-    public int[] getTriangleSides(int[] segments) {
-        int out[] = new int[0];
-        if (segments == null || segments.length < 3)
-            return out;
-        Arrays.sort(segments);
+    public int triangleNumber(int[] nums) {
+        if (nums == null || nums.length < 3)
+            return 0;
+        Arrays.sort(nums);
         
-        for (int i = 2; i < segments.length; i++) {
-            if (segments[i] < segments[i-1] + segments[i-2]) {
-                out = new int[3];
-                for (int j = 0; j < 3; j++)
-                    out[j] = segments[i-j];
-                return out;
+        int count = 0;
+        
+        for (int i = 2; i < nums.length; i++) {
+            int left = 0;
+            int right = i - 1;
+            while  (left < right) {
+                if (nums[left] + nums[right] <= nums[i])
+                    left++;
+                else {
+                    count += right - left;
+                    right--;
+                }
             }
         }
         
-        return out;
+        return count;
+        
     }
 
 }
