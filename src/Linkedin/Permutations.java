155479e87ceddf02bb20ca3e9df1bfff7ab74e1d
qa-mbp-b-2 <qa-mbp-b-2@192.168.0.62>
2017-10-09 16:24:36 -0700

    Leetcode 31 next Permutation

--- src/Linkedin/Permutations.java
+++ src/Linkedin/Permutations.java
@@ -48,6 +48,7 @@ public class Permutations {
         
     }
     
+    // in space swap solution
     private void permS(int nums[], int idx, List<List<Integer>> result) {
         if (idx >= nums.length) {
             List<Integer> t = new ArrayList<>();
@@ -115,6 +116,75 @@ public class Permutations {
         return result;
     }
     
+    /*
+     * Leetcode 31. Next Permutation
+     * Implement next permutation, which rearranges numbers into the lexicographically 
+     * next greater permutation of numbers.
+
+        If such arrangement is not possible, it must rearrange it as the lowest possible 
+        order (ie, sorted in ascending order).
+        
+        The replacement must be in-place, do not allocate extra memory.
+        
+        Here are some examples. Inputs are in the left-hand column and its corresponding 
+        outputs are in the right-hand column.
+        1,2,3 -> 1,3,2
+        3,2,1 -> 1,2,3
+        1,1,5 -> 1,5,1
+     */
+    private void reverse(int nums[], int left, int right) {
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+        
+    }
+    
+    public void nextPermutation(int[] nums) {
+        if (nums == null || nums.length < 2)
+            return;
+        int len = nums.length;
+        // scan the array from right to left, find the first Descending number 
+        int firstDescending = len-2;
+        
+        while (firstDescending >= 0 && nums[firstDescending] >= nums[firstDescending+1])
+            firstDescending--;
+        // if first Descending number is -1, 
+        // it means the whole array is sorted in Descending order
+        // like 4,3,2,1,  so the next should be 1,2,3,4
+        // reverse the whole array
+        if (firstDescending == -1) {
+            reverse(nums, 0, len-1);
+            return;
+        }
+        
+        // scan the array from right to left, 
+        // find the first number less than first Descending number 
+        int firstSwap = len-1;
+        while (nums[firstDescending] >= nums[firstSwap])
+            firstSwap --;
+        // swap this 2 numbers
+        // example 6，3，4，9，8，7，1
+        // first Descending number is 4
+        // first number need to be swap is 7
+        // after swap it will be 6，3，7，9，8，4，1
+        // the number after swap point will be Descending
+        // 9，8，4，1
+        swap(nums,firstDescending,firstSwap);
+        // reverse that part, and we got the result
+        reverse(nums, firstDescending+1, len-1);
+        
+    }
+    
+    public static void main(String args[]) {
+        Permutations p = new Permutations();
+        int a1[] = {1, 2};
+        p.nextPermutation(a1);
+        for (int i:a1)
+            System.out.print(i+" ");
+        System.out.println();
+    }
     
 
 }
