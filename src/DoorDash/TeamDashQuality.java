47199883261be89c0988f5907df9c422b6ea559c
ghost2004 <liutg@hotmail.com>
2020-11-13 17:01:54 -0800

    Leetcode 1383. Maximum Performance of a Team

--- src/DoorDash/TeamDashQuality.java
+++ src/DoorDash/TeamDashQuality.java
@@ -20,6 +20,35 @@ import java.util.*;
  *  This is the highest quality that can be achieved, so the answer is 78.
  *  
  *  
+ *  Same to  * Leetcode 1383. Maximum Performance of a Team
+ * 
+ * There are n engineers numbered from 1 to n and two arrays: speed and efficiency, where speed[i] and efficiency[i] represent the speed 
+ * and efficiency for the i-th engineer respectively. Return the maximum performance of a team composed of at most k engineers, since 
+ * the answer can be a huge number, return this modulo 10^9 + 7.
+
+The performance of a team is the sum of their engineers' speeds multiplied by the minimum efficiency among their engineers. 
+
+ 
+
+Example 1:
+
+Input: n = 6, speed = [2,10,3,1,5,8], efficiency = [5,4,3,9,7,2], k = 2
+Output: 60
+Explanation: 
+We have the maximum performance of the team by selecting engineer 2 (with speed=10 and efficiency=4) and engineer 5 (with speed=5 and efficiency=7).
+ That is, performance = (10 + 5) * min(4, 7) = 60.
+Example 2:
+
+Input: n = 6, speed = [2,10,3,1,5,8], efficiency = [5,4,3,9,7,2], k = 3
+Output: 68
+Explanation:
+This is the same example as the first but k = 3. We can select engineer 1, engineer 2 and engineer 5 to get the maximum performance of the team.
+ That is, performance = (2 + 10 + 5) * min(5, 4, 7) = 68.
+Example 3:
+
+Input: n = 6, speed = [2,10,3,1,5,8], efficiency = [5,4,3,9,7,2], k = 4
+Output: 72
+ *  
  */
 public class TeamDashQuality {
     static class Dasher {
@@ -52,8 +81,8 @@ public class TeamDashQuality {
             }
         });
         
-        int max = 0;
-        int curSpeedTotal = 0;
+        long max = 0;
+        long curSpeedTotal = 0;
         for (int i = 0; i < n; i++) {
             if (heap.size() >= maxDashers) {
                 Dasher out = heap.poll();
@@ -64,7 +93,7 @@ public class TeamDashQuality {
             max = Math.max(max, curSpeedTotal * dashers[i]._professionalism);
         }
         
-        return max;
+        return  (int) (max % (long)(1e9 + 7));
     }
     
     public static void main(String args[]) {
@@ -77,5 +106,13 @@ public class TeamDashQuality {
         int speed3[] = {12, 112, 100, 13, 55};
         int pro3[] = {31, 4, 100, 55, 50};
         System.out.println(computeQuality(5, speed3, pro3, 3));
+        
+        int speed4[] = {2,10,3,1,5,8};
+        int pro4[] = {5,4,3,9,7,2};
+        System.out.println(computeQuality(6, speed4, pro4, 2));
+
+        System.out.println(computeQuality(6, speed4, pro4, 3));
+
+        System.out.println(computeQuality(6, speed4, pro4, 4));
     }
 }
