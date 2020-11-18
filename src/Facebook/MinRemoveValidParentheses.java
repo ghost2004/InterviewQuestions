76fce0bc42245a79b51b54abbba6a43cc47a8a72
ghost2004 <liutg@hotmail.com>
2020-11-17 23:16:06 -0800

    Leetcode 1249. Minimum Remove to Make Valid Parentheses

---
+++ src/Facebook/MinRemoveValidParentheses.java
@@ -0,0 +1,65 @@
+package Facebook;
+
+/*
+ * Leetcode 1249. Minimum Remove to Make Valid Parentheses
+ * Given a string s of '(' , ')' and lowercase English characters. 
+
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and 
+return any valid string.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+ 
+
+Example 1:
+
+Input: s = "lee(t(c)o)de)"
+Output: "lee(t(c)o)de"
+Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+Example 2:
+
+Input: s = "a)b(c)d"
+Output: "ab(c)d"
+Example 3:
+
+Input: s = "))(("
+Output: ""
+Explanation: An empty string is also valid.
+Example 4:
+
+Input: s = "(a(b(c)d)"
+Output: "a(b(c)d)"
+ */
+import java.util.*;
+public class MinRemoveValidParentheses {
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(i);
+            }
+            if (c == ')') {
+                if (!stack.isEmpty() && stack.peek() >= 0) {
+                    if (stack.peek() == 0 && s.charAt(0) == ')')
+                        stack.push(-i);
+                    else
+                    stack.pop();
+                } else {
+                    stack.push(-i);
+                }
+            }
+        }
+        
+        while (!stack.isEmpty()) {
+            sb.deleteCharAt(Math.abs(stack.pop()));
+        }
+        return sb.toString();
+        
+    }
+}
